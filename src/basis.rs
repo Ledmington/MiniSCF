34f107d61d762a1ef03eb1e7d368662ec1ab1889
Ledmington <ledmington.dev@gmail.com>
2026-06-21 11:48:21 +0200

    Refactoring BasisSet

---
+++ src/basis.rs
@@ -0,0 +1,296 @@
+use std::f64::consts::PI;
+
+use ndarray::Array2;
+
+#[derive(Clone, Copy)]
+pub(crate) struct Point {
+    pub(crate) x: f64,
+    pub(crate) y: f64,
+    pub(crate) z: f64,
+}
+
+impl Point {
+    fn sub(&self, p: &Point) -> Point {
+        Point {
+            x: self.x - p.x,
+            y: self.y - p.y,
+            z: self.z - p.z,
+        }
+    }
+
+    fn norm_squared(&self) -> f64 {
+        self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
+    }
+}
+
+struct PrimitiveGaussian {
+    normalization_constant: f64,
+    gaussian_exponent: f64,
+    center: Point,
+}
+
+impl PrimitiveGaussian {
+    fn new(alpha: f64, center: Point) -> Self {
+        PrimitiveGaussian {
+            normalization_constant: get_normalization_term(alpha),
+            gaussian_exponent: alpha,
+            center,
+        }
+    }
+
+    // pub fn compute(&self, r: &Point) -> f64 {
+    //     let dx = r.x - self.center.x;
+    //     let dy = r.y - self.center.y;
+    //     let dz = r.z - self.center.z;
+    //     let r2 = dx * dx + dy * dy + dz * dz;
+    //     self.normalization_constant * (-self.gaussian_exponent * r2).exp()
+    // }
+}
+
+struct ContractedGaussian {
+    coefficients: Vec<f64>,
+    primitives: Vec<PrimitiveGaussian>,
+}
+
+impl ContractedGaussian {
+    fn new(coefficients: &[f64], alpha: &[f64], center: &Point) -> Self {
+        assert_eq!(coefficients.len(), alpha.len());
+        ContractedGaussian {
+            coefficients: coefficients.to_vec(),
+            primitives: alpha
+                .iter()
+                .map(|a| PrimitiveGaussian::new(*a, *center))
+                .collect(),
+        }
+    }
+
+    // pub fn compute(&self, r: &Point) -> f64 {
+    //     self.primitives.iter().map(|p| p.compute(r)).sum()
+    // }
+}
+
+fn get_normalization_term(alpha: f64) -> f64 {
+    ((2.0 * alpha) / PI).powf(3.0 / 4.0)
+}
+
+pub(crate) struct BasisSet {
+    contracted_gaussians: Vec<ContractedGaussian>,
+}
+
+impl BasisSet {
+    pub(crate) fn new(coefficients: &[f64], alpha: &[f64], centers: &[Point]) -> Self {
+        assert_eq!(coefficients.len(), alpha.len());
+        BasisSet {
+            contracted_gaussians: centers
+                .iter()
+                .map(|p| ContractedGaussian::new(coefficients, alpha, p))
+                .collect(),
+        }
+    }
+
+    pub(crate) fn num_contracted_gaussians(&self) -> usize {
+        self.contracted_gaussians.len()
+    }
+
+    pub(crate) fn compute_contracted_gaussians_overlap(&self, m: &mut Array2<f64>) -> () {
+        for (i, a) in self.contracted_gaussians.iter().enumerate() {
+            for (j, b) in self.contracted_gaussians.iter().enumerate() {
+                m[[i, j]] = Self::compute_overlap(&a, &b);
+            }
+        }
+    }
+
+    fn compute_overlap(a: &ContractedGaussian, b: &ContractedGaussian) -> f64 {
+        let mut s = 0.0;
+        for (i, prim_a) in a.primitives.iter().enumerate() {
+            let d_i = a.coefficients[i];
+            for (j, prim_b) in b.primitives.iter().enumerate() {
+                let d_j = b.coefficients[j];
+                s += d_i * d_j * Self::compute_primitive_gaussians_overlap(prim_a, prim_b);
+            }
+        }
+        s
+    }
+
+    fn compute_primitive_gaussians_overlap(a: &PrimitiveGaussian, b: &PrimitiveGaussian) -> f64 {
+        let p = a.gaussian_exponent + b.gaussian_exponent;
+        let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
+        let r2 = a.center.sub(&b.center).norm_squared();
+        a.normalization_constant
+            * b.normalization_constant
+            * (PI / p).powf(3.0 / 2.0)
+            * (-mu * r2).exp()
+    }
+
+    pub(crate) fn compute_contracted_gaussians_kinetic_energy(&self, m: &mut Array2<f64>) -> () {
+        for (i, a) in self.contracted_gaussians.iter().enumerate() {
+            for (j, b) in self.contracted_gaussians.iter().enumerate() {
+                m[[i, j]] = Self::compute_kinetic_energy(&a, &b);
+            }
+        }
+    }
+
+    fn compute_kinetic_energy(a: &ContractedGaussian, b: &ContractedGaussian) -> f64 {
+        let mut s = 0.0;
+        for (i, prim_a) in a.primitives.iter().enumerate() {
+            let d_i = a.coefficients[i];
+            for (j, prim_b) in b.primitives.iter().enumerate() {
+                let d_j = b.coefficients[j];
+                s += d_i * d_j * Self::compute_primitive_gaussians_kinetic_energy(prim_a, prim_b);
+            }
+        }
+        s
+    }
+
+    fn compute_primitive_gaussians_kinetic_energy(
+        a: &PrimitiveGaussian,
+        b: &PrimitiveGaussian,
+    ) -> f64 {
+        let p = a.gaussian_exponent + b.gaussian_exponent;
+        let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
+        let r2 = a.center.sub(&b.center).norm_squared();
+        a.normalization_constant
+            * b.normalization_constant
+            * (PI / p).powf(3.0 / 2.0)
+            * (-mu * r2).exp()
+            * mu
+            * (3.0 - 2.0 * mu * r2)
+    }
+
+    pub(crate) fn compute_contracted_gaussians_nuclear_attraction(
+        &self,
+        m: &mut Array2<f64>,
+    ) -> () {
+        for (i, a) in self.contracted_gaussians.iter().enumerate() {
+            for (j, b) in self.contracted_gaussians.iter().enumerate() {
+                m[[i, j]] = Self::compute_nuclear_attraction(&a, &b);
+            }
+        }
+    }
+
+    fn compute_nuclear_attraction(a: &ContractedGaussian, b: &ContractedGaussian) -> f64 {
+        let mut s = 0.0;
+        for (i, prim_a) in a.primitives.iter().enumerate() {
+            let d_i = a.coefficients[i];
+            for (j, prim_b) in b.primitives.iter().enumerate() {
+                let d_j = b.coefficients[j];
+                s += d_i
+                    * d_j
+                    * Self::compute_primitive_gaussians_nuclear_attraction(prim_a, prim_b);
+            }
+        }
+        s
+    }
+
+    fn compute_primitive_gaussians_nuclear_attraction(
+        a: &PrimitiveGaussian,
+        b: &PrimitiveGaussian,
+    ) -> f64 {
+        let p = a.gaussian_exponent + b.gaussian_exponent;
+        let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
+        let r2 = a.center.sub(&b.center).norm_squared();
+        -(a.normalization_constant
+            * b.normalization_constant
+            * ((2.0 * PI) / p)
+            * (-mu * r2).exp()
+            * boys_0(p * r2))
+    }
+
+    pub(crate) fn compute_electron_repulsion(&self, eri: &mut Vec<Vec<Vec<Vec<f64>>>>) -> () {
+        for (a, contr_gauss_a) in self.contracted_gaussians.iter().enumerate() {
+            for (b, contr_gauss_b) in self.contracted_gaussians.iter().enumerate() {
+                for (c, contr_gauss_c) in self.contracted_gaussians.iter().enumerate() {
+                    for (d, contr_gauss_d) in self.contracted_gaussians.iter().enumerate() {
+                        let mut sum = 0.0;
+
+                        for i in 0..3 {
+                            for j in 0..3 {
+                                for k in 0..3 {
+                                    for l in 0..3 {
+                                        let d_i = contr_gauss_a.coefficients[i];
+                                        let d_j = contr_gauss_b.coefficients[j];
+                                        let d_k = contr_gauss_c.coefficients[k];
+                                        let d_l = contr_gauss_d.coefficients[l];
+
+                                        sum += d_i
+                                            * d_j
+                                            * d_k
+                                            * d_l
+                                            * Self::primitive_eri(
+                                                &contr_gauss_a.primitives[i],
+                                                &contr_gauss_b.primitives[j],
+                                                &contr_gauss_c.primitives[k],
+                                                &contr_gauss_d.primitives[l],
+                                            );
+                                    }
+                                }
+                            }
+                        }
+
+                        eri[a][b][c][d] = sum;
+                    }
+                }
+            }
+        }
+    }
+
+    fn primitive_eri(
+        a: &PrimitiveGaussian,
+        b: &PrimitiveGaussian,
+        c: &PrimitiveGaussian,
+        d: &PrimitiveGaussian,
+    ) -> f64 {
+        let r_ab_2 = a.center.sub(&b.center).norm_squared();
+        let r_cd_2 = c.center.sub(&d.center).norm_squared();
+        let p = a.gaussian_exponent + b.gaussian_exponent;
+        let q = c.gaussian_exponent + d.gaussian_exponent;
+        let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
+        let v = (c.gaussian_exponent * d.gaussian_exponent) / q;
+        let p_center = Point {
+            x: (a.gaussian_exponent * a.center.x + b.gaussian_exponent * b.center.x) / p,
+            y: (a.gaussian_exponent * a.center.y + b.gaussian_exponent * b.center.y) / p,
+            z: (a.gaussian_exponent * a.center.z + b.gaussian_exponent * b.center.z) / p,
+        };
+        let q_center = Point {
+            x: (c.gaussian_exponent * c.center.x + d.gaussian_exponent * d.center.x) / q,
+            y: (c.gaussian_exponent * c.center.y + d.gaussian_exponent * d.center.y) / q,
+            z: (c.gaussian_exponent * c.center.z + d.gaussian_exponent * d.center.z) / q,
+        };
+        let r_pq_2 = p_center.sub(&q_center).norm_squared();
+        let t = ((p * q) / (p + q)) * r_pq_2;
+        a.normalization_constant
+            * b.normalization_constant
+            * c.normalization_constant
+            * d.normalization_constant
+            * ((2.0 * PI.powf(5.0 / 2.0)) / (p * q * (p + q).sqrt()))
+            * (-mu * r_ab_2).exp()
+            * (-v * r_cd_2).exp()
+            * boys_0(t)
+    }
+}
+
+fn erf(x: f64) -> f64 {
+    // constants
+    let a1 = 0.254829592;
+    let a2 = -0.284496736;
+    let a3 = 1.421413741;
+    let a4 = -1.453152027;
+    let a5 = 1.061405429;
+    let p = 0.3275911;
+
+    let sign = if x < 0.0 { -1.0 } else { 1.0 };
+    let x = x.abs();
+
+    // Abramowitz-Stegun formula
+    let t = 1.0 / (1.0 + p * x);
+    let y = 1.0 - (((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t) * (-x * x).exp();
+
+    sign * y
+}
+
+fn boys_0(t: f64) -> f64 {
+    if t < 1.0e-8 {
+        return 1.0;
+    }
+    0.5 * (PI / t).sqrt() * erf(t.sqrt())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,244 +1,11 @@
 #![forbid(unsafe_code)]
 
-use std::f64::consts::PI;
+mod basis;
 
 use ndarray::{Array1, Array2};
 use ndarray_linalg::{Eigh, UPLO};
 
-#[derive(Clone, Copy)]
-struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-impl Point {
-    fn sub(&self, p: &Point) -> Point {
-        Point {
-            x: self.x - p.x,
-            y: self.y - p.y,
-            z: self.z - p.z,
-        }
-    }
-
-    fn norm_squared(&self) -> f64 {
-        self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
-    }
-}
-
-struct PrimitiveGaussian {
-    normalization_constant: f64,
-    gaussian_exponent: f64,
-    center: Point,
-}
-
-impl PrimitiveGaussian {
-    fn new(alpha: f64, center: Point) -> Self {
-        PrimitiveGaussian {
-            normalization_constant: get_normalization_term(alpha),
-            gaussian_exponent: alpha,
-            center,
-        }
-    }
-
-    // pub fn compute(&self, r: &Point) -> f64 {
-    //     let dx = r.x - self.center.x;
-    //     let dy = r.y - self.center.y;
-    //     let dz = r.z - self.center.z;
-    //     let r2 = dx * dx + dy * dy + dz * dz;
-    //     self.normalization_constant * (-self.gaussian_exponent * r2).exp()
-    // }
-}
-
-struct ContractedGaussian {
-    coefficients: Vec<f64>,
-    primitives: Vec<PrimitiveGaussian>,
-}
-
-impl ContractedGaussian {
-    fn new(coefficients: &[f64], alpha: &[f64], center: &Point) -> Self {
-        assert_eq!(coefficients.len(), alpha.len());
-        ContractedGaussian {
-            coefficients: coefficients.to_vec(),
-            primitives: alpha
-                .iter()
-                .map(|a| PrimitiveGaussian::new(*a, *center))
-                .collect(),
-        }
-    }
-
-    // pub fn compute(&self, r: &Point) -> f64 {
-    //     self.primitives.iter().map(|p| p.compute(r)).sum()
-    // }
-}
-
-fn get_normalization_term(alpha: f64) -> f64 {
-    ((2.0 * alpha) / PI).powf(3.0 / 4.0)
-}
-
-struct BasisSet {
-    contracted_gaussians: Vec<ContractedGaussian>,
-}
-
-impl BasisSet {
-    fn new(coefficients: &[f64], alpha: &[f64], centers: &[Point]) -> Self {
-        assert_eq!(coefficients.len(), alpha.len());
-        BasisSet {
-            contracted_gaussians: centers
-                .iter()
-                .map(|p| ContractedGaussian::new(coefficients, alpha, p))
-                .collect(),
-        }
-    }
-
-    // fn contracted_overlap(&self) -> f64 {
-    //     let mut s = 0.0;
-    //     for a in &self.contracted_gaussians {
-    //         for b in &self.contracted_gaussians {
-    //             s += compute_contracted_gaussians_overlap(a, b);
-    //         }
-    //     }
-    //     s
-    // }
-}
-
-fn compute_contracted_gaussians_overlap(a: &ContractedGaussian, b: &ContractedGaussian) -> f64 {
-    let mut s = 0.0;
-    for (i, prim_a) in a.primitives.iter().enumerate() {
-        let d_i = a.coefficients[i];
-        for (j, prim_b) in b.primitives.iter().enumerate() {
-            let d_j = b.coefficients[j];
-            s += d_i * d_j * compute_primitive_gaussians_overlap(prim_a, prim_b);
-        }
-    }
-    s
-}
-
-fn compute_primitive_gaussians_overlap(a: &PrimitiveGaussian, b: &PrimitiveGaussian) -> f64 {
-    let p = a.gaussian_exponent + b.gaussian_exponent;
-    let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
-    let r2 = a.center.sub(&b.center).norm_squared();
-    a.normalization_constant
-        * b.normalization_constant
-        * (PI / p).powf(3.0 / 2.0)
-        * (-mu * r2).exp()
-}
-
-fn compute_contracted_gaussians_kinetic_energy(
-    a: &ContractedGaussian,
-    b: &ContractedGaussian,
-) -> f64 {
-    let mut s = 0.0;
-    for (i, prim_a) in a.primitives.iter().enumerate() {
-        let d_i = a.coefficients[i];
-        for (j, prim_b) in b.primitives.iter().enumerate() {
-            let d_j = b.coefficients[j];
-            s += d_i * d_j * compute_primitive_gaussians_kinetic_energy(prim_a, prim_b);
-        }
-    }
-    s
-}
-
-fn compute_primitive_gaussians_kinetic_energy(a: &PrimitiveGaussian, b: &PrimitiveGaussian) -> f64 {
-    let p = a.gaussian_exponent + b.gaussian_exponent;
-    let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
-    let r2 = a.center.sub(&b.center).norm_squared();
-    a.normalization_constant
-        * b.normalization_constant
-        * (PI / p).powf(3.0 / 2.0)
-        * (-mu * r2).exp()
-        * mu
-        * (3.0 - 2.0 * mu * r2)
-}
-
-fn erf(x: f64) -> f64 {
-    // constants
-    let a1 = 0.254829592;
-    let a2 = -0.284496736;
-    let a3 = 1.421413741;
-    let a4 = -1.453152027;
-    let a5 = 1.061405429;
-    let p = 0.3275911;
-
-    let sign = if x < 0.0 { -1.0 } else { 1.0 };
-    let x = x.abs();
-
-    // Abramowitz-Stegun formula
-    let t = 1.0 / (1.0 + p * x);
-    let y = 1.0 - (((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t) * (-x * x).exp();
-
-    sign * y
-}
-
-fn boys_0(t: f64) -> f64 {
-    if t < 1.0e-8 {
-        return 1.0;
-    }
-    0.5 * (PI / t).sqrt() * erf(t.sqrt())
-}
-
-fn compute_contracted_gaussians_nuclear_attraction(
-    a: &ContractedGaussian,
-    b: &ContractedGaussian,
-) -> f64 {
-    let mut s = 0.0;
-    for (i, prim_a) in a.primitives.iter().enumerate() {
-        let d_i = a.coefficients[i];
-        for (j, prim_b) in b.primitives.iter().enumerate() {
-            let d_j = b.coefficients[j];
-            s += d_i * d_j * compute_primitive_gaussians_nuclear_attraction(prim_a, prim_b);
-        }
-    }
-    s
-}
-
-fn compute_primitive_gaussians_nuclear_attraction(
-    a: &PrimitiveGaussian,
-    b: &PrimitiveGaussian,
-) -> f64 {
-    let p = a.gaussian_exponent + b.gaussian_exponent;
-    let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
-    let r2 = a.center.sub(&b.center).norm_squared();
-    -(a.normalization_constant
-        * b.normalization_constant
-        * ((2.0 * PI) / p)
-        * (-mu * r2).exp()
-        * boys_0(p * r2))
-}
-
-fn primitive_eri(
-    a: &PrimitiveGaussian,
-    b: &PrimitiveGaussian,
-    c: &PrimitiveGaussian,
-    d: &PrimitiveGaussian,
-) -> f64 {
-    let r_ab_2 = a.center.sub(&b.center).norm_squared();
-    let r_cd_2 = c.center.sub(&d.center).norm_squared();
-    let p = a.gaussian_exponent + b.gaussian_exponent;
-    let q = c.gaussian_exponent + d.gaussian_exponent;
-    let mu = (a.gaussian_exponent * b.gaussian_exponent) / p;
-    let v = (c.gaussian_exponent * d.gaussian_exponent) / q;
-    let p_center = Point {
-        x: (a.gaussian_exponent * a.center.x + b.gaussian_exponent * b.center.x) / p,
-        y: (a.gaussian_exponent * a.center.y + b.gaussian_exponent * b.center.y) / p,
-        z: (a.gaussian_exponent * a.center.z + b.gaussian_exponent * b.center.z) / p,
-    };
-    let q_center = Point {
-        x: (c.gaussian_exponent * c.center.x + d.gaussian_exponent * d.center.x) / q,
-        y: (c.gaussian_exponent * c.center.y + d.gaussian_exponent * d.center.y) / q,
-        z: (c.gaussian_exponent * c.center.z + d.gaussian_exponent * d.center.z) / q,
-    };
-    let r_pq_2 = p_center.sub(&q_center).norm_squared();
-    let t = ((p * q) / (p + q)) * r_pq_2;
-    a.normalization_constant
-        * b.normalization_constant
-        * c.normalization_constant
-        * d.normalization_constant
-        * ((2.0 * PI.powf(5.0 / 2.0)) / (p * q * (p + q).sqrt()))
-        * (-mu * r_ab_2).exp()
-        * (-v * r_cd_2).exp()
-        * boys_0(t)
-}
+use crate::basis::{BasisSet, Point};
 
 fn approx_eq(a: f64, b: f64, tol: f64) -> bool {
     (a - b).abs() <= tol
@@ -286,29 +53,16 @@ fn main() {
         ],
     );
 
-    let n = sto_3g.contracted_gaussians.len();
+    let n = sto_3g.num_contracted_gaussians();
 
     // Build S, T, V as Array2
     let mut s = Array2::<f64>::zeros((n, n));
     let mut t = Array2::<f64>::zeros((n, n));
     let mut v = Array2::<f64>::zeros((n, n));
 
-    for i in 0..n {
-        for j in 0..n {
-            s[[i, j]] = compute_contracted_gaussians_overlap(
-                &sto_3g.contracted_gaussians[i],
-                &sto_3g.contracted_gaussians[j],
-            );
-            t[[i, j]] = compute_contracted_gaussians_kinetic_energy(
-                &sto_3g.contracted_gaussians[i],
-                &sto_3g.contracted_gaussians[j],
-            );
-            v[[i, j]] = compute_contracted_gaussians_nuclear_attraction(
-                &sto_3g.contracted_gaussians[i],
-                &sto_3g.contracted_gaussians[j],
-            );
-        }
-    }
+    sto_3g.compute_contracted_gaussians_overlap(&mut s);
+    sto_3g.compute_contracted_gaussians_kinetic_energy(&mut t);
+    sto_3g.compute_contracted_gaussians_nuclear_attraction(&mut v);
 
     println!("Overlap (S):\n{s:?}\n");
 
@@ -335,68 +89,35 @@ fn main() {
     let mut eri: Vec<Vec<Vec<Vec<f64>>>> = vec![
         vec![
             vec![
-                vec![0.0; sto_3g.contracted_gaussians.len()];
-                sto_3g.contracted_gaussians.len()
+                vec![0.0; sto_3g.num_contracted_gaussians()];
+                sto_3g.num_contracted_gaussians()
             ];
-            sto_3g.contracted_gaussians.len()
+            sto_3g.num_contracted_gaussians()
         ];
-        sto_3g.contracted_gaussians.len()
+        sto_3g.num_contracted_gaussians()
     ];
-    for (a, contr_gauss_a) in sto_3g.contracted_gaussians.iter().enumerate() {
-        for (b, contr_gauss_b) in sto_3g.contracted_gaussians.iter().enumerate() {
-            for (c, contr_gauss_c) in sto_3g.contracted_gaussians.iter().enumerate() {
-                for (d, contr_gauss_d) in sto_3g.contracted_gaussians.iter().enumerate() {
-                    let mut sum = 0.0;
 
-                    for i in 0..3 {
-                        for j in 0..3 {
-                            for k in 0..3 {
-                                for l in 0..3 {
-                                    let d_i = contr_gauss_a.coefficients[i];
-                                    let d_j = contr_gauss_b.coefficients[j];
-                                    let d_k = contr_gauss_c.coefficients[k];
-                                    let d_l = contr_gauss_d.coefficients[l];
-
-                                    sum += d_i
-                                        * d_j
-                                        * d_k
-                                        * d_l
-                                        * primitive_eri(
-                                            &contr_gauss_a.primitives[i],
-                                            &contr_gauss_b.primitives[j],
-                                            &contr_gauss_c.primitives[k],
-                                            &contr_gauss_d.primitives[l],
-                                        );
-                                }
-                            }
-                        }
-                    }
-
-                    eri[a][b][c][d] = sum;
-                }
-            }
-        }
-    }
+    sto_3g.compute_electron_repulsion(&mut eri);
 
     for (a, eri_a) in eri
         .iter()
         .enumerate()
-        .take(sto_3g.contracted_gaussians.len())
+        .take(sto_3g.num_contracted_gaussians())
     {
         for (b, eri_a_b) in eri_a
             .iter()
             .enumerate()
-            .take(sto_3g.contracted_gaussians.len())
+            .take(sto_3g.num_contracted_gaussians())
         {
             for (c, eri_a_b_c) in eri_a_b
                 .iter()
                 .enumerate()
-                .take(sto_3g.contracted_gaussians.len())
+                .take(sto_3g.num_contracted_gaussians())
             {
                 for (d, eri_a_b_c_d) in eri_a_b_c
                     .iter()
                     .enumerate()
-                    .take(sto_3g.contracted_gaussians.len())
+                    .take(sto_3g.num_contracted_gaussians())
                 {
                     println!("⟨{a}{b}|{c}{d}⟩ = {eri_a_b_c_d}");
                 }
